200541563192334b3f4f5f3779edf47ce82b3131
William Papineau <wpapineau@nexient.com>
2022-09-27 18:50:00 -0400

    adds coffee list and data service

--- Angular-TDD-Kata/src/app/app.module.ts
+++ Angular-TDD-Kata/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoffeeListComponent } from './coffee-list/coffee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoffeeListComponent
   ],
   imports: [
     BrowserModule
